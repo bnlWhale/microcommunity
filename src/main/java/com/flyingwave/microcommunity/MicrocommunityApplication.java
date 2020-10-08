bbd6b75c37429778a4a30f6e656bd3abfbfe2ded
bnl <ducounau@gmail.com>
2020-10-08 00:14:12 -0700

    add module

---
+++ src/main/java/com/flyingwave/microcommunity/MicrocommunityApplication.java
@@ -0,0 +1,13 @@
+package com.flyingwave.microcommunity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicrocommunityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicrocommunityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/flyingwave/microcommunity/MicrocommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flyingwave.microcommunity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicrocommunityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
